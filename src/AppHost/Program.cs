331fdfb7b1031c2f34cfb24012b488abcb17ac7a
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2025-02-27 16:40:18 +1100

    make aspire database persistent

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,10 +1,15 @@
+using System.Reflection.Metadata;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var passwordParameter = builder.AddParameter("sql-password", "Password.1");
 var sqlserver = builder
-    .AddSqlServer("sqlserver")
-    .WithLifetime(ContainerLifetime.Persistent); // persist container between restarts
+    .AddSqlServer("sqlserver", password: passwordParameter, port: 9999)
+    .WithDataVolume() // persists data between restarts
+    .WithLifetime(ContainerLifetime.Persistent); // decreases startup time
 
-var database = sqlserver.AddDatabase("database");
+var database = sqlserver
+    .AddDatabase("database");
 
 builder.AddProject<Projects.Web_Api>("Web-Api")
     .WithReference(database)
